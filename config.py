cac42e89943a3563fdf5b27003ae61131e87d69d
Behzod031 <behzod031@gmail.com>
2025-06-19 12:26:38 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,12 @@
-API_TOKEN = "7668495123:AAER2aqqi4gOOrDm11RTdXfQUoFl4VQeMAs"
+API_TOKEN = "7719820520:AAGy0QDOjOBSa76AhKkIpv_HE5JHc7THxKM"
 OPERATOR_ID = 7112971881  # ← замени на Telegram user_id
 
-# Путь к tesseract на Windows
-TESSERACT_PATH = None
+
+# Абсолютный путь до tesseract.exe
+TESSERACT_PATH = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+
 
 ALBUM_TIMEOUT = 3                     # Секунды ожидания перед обработкой альбома
 
 # Вебхук для Битрикс24
-BITRIX24_WEBHOOK_URL = "https://xonsaroy.bitrix24.ru/rest/57183/j31h0z6dan0uv8e3/"
+BITRIX24_WEBHOOK_URL = "https://xonsaroy.bitrix24.ru/rest/57183/nurmhsapdkh117bh"
